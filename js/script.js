37b6ec6cc183c8fc7fe536fbedb9ad20bb8b694e
milton <milsmsramirez@gmail.com>
2015-02-02 01:07:10 -0600

    prototipo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 Parse.initialize("YJwHjwYF08scjC2SEdl9Ve2YFB3056FQfkUgGu8p", "awDPDR2YNfZNtbJ54VzEZPiWJZmLekU8qkJxnlOw");
 
-angular.module('AuthApp', [])
+var  AuthApp = angular.module('AuthApp', [])
 .run(['$rootScope', function($scope) {
   $scope.scenario = 'Sign up';
   $scope.currentUser = Parse.User.current();
@@ -39,3 +39,17 @@ angular.module('AuthApp', [])
     $scope.currentUser = null;
   };
 }]);
+
+AuthApp.filter("pictureSong", function(){
+  return function(item){
+    return (JSON.stringify(item)).slice(10,-18);
+  };
+});
+AuthApp.controller('SearchController', function ($scope, $http) {
+  $http.get('http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=85b8c37b1a6be5182a5ed0549c4a7400&format=json').success(function(data) {
+    $scope.phones = data.tracks.track;
+  });
+  $scope.orderBy = function(orden){
+    $scope.orderSelect = orden;
+  };
+});
